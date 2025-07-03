83bc4a601cd56ca9456a96f03d4beefc37d398ee
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-07-03 11:44:07 -0600

    feat: Add Swagger UI integration and API documentation for Notes API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const notesRoutes = require('./routes/notesRouter.js');
 const userRoutes = require('./routes/userRouter.js');
 const loginRoutes = require('./routes/loginRouter.js');
 
-
 const app = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +19,6 @@ app.use(cors());
 app.use('/api/notes', notesRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', loginRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
